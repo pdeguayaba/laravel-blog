ce11f8745facec3150e3aac947f066c4f63e935d
Edwin J. Estrella Ayala <edwinjestrella@yahoo.com>
2020-05-14 08:29:36 -0400

    Layout setup

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    </head>
-    <body>
-        <div class="container">
-        <h1>Post {{ $id }} {{ $name }}</h1>
-    </body>
-</html>
+@section('content')
+    <h1>Post Page {{ $id }} {{ $name }}</h1>
+@stop
